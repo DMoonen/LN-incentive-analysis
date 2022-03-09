ec2e18f896dfe9e936cb929eef589ee7a0c68d7f
DMoonen <Jackos124@gmail.com>
2022-03-09 16:24:28 +0100

    Add strategies

---
+++ placement_strategies.py
@@ -0,0 +1,33 @@
+import networkx as nx
+
+""" Channel creation strategies """
+
+
+def uniform_random(graph, n):
+    # Todo
+    return graph
+
+
+def highest_degree(graph, n):
+    # Todo
+    return graph
+
+
+def betweenness_centrality(graph, n):
+    # Todo
+    return graph
+
+
+def k_center(graph, n):
+    # Todo
+    return graph
+
+
+def k_means(graph, n):
+    # Todo
+    return graph
+
+
+def fee_weighted_centrality(graph, n):
+    # Todo
+    return graph
