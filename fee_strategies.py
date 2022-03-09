ac6403e4af6944302b207727b84b0e169486daf1
DMoonen <Jackos124@gmail.com>
2022-03-09 16:24:36 +0100

    Add fee strategies

---
+++ fee_strategies.py
@@ -0,0 +1,13 @@
+import networkx as nx
+
+""" Fee optimization strategies """
+
+
+def graph_fee_optimization(graph):
+    # Todo
+    return graph
+
+
+def edge_fee_optimization(edge):
+    # Todo
+    return edge
